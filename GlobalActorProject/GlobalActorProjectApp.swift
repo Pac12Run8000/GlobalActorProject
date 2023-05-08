2ef0ea7c0788639183524e8e5f3a9baa4f4706c7
Pac12Run8000 <gangstarr78992@>
2023-05-08 12:09:47 -0700

    Initial Commit

---
+++ GlobalActorProject/GlobalActorProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalActorProjectApp.swift
+//  GlobalActorProject
+//
+//  Created by Michelle Grover on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct GlobalActorProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
